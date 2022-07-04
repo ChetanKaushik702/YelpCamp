bd3eef0b6846c61cecfb754f54d98742ecfb8708
Chetan Kaushik <kaushikchetan199@gmail.com>
2022-07-04 21:49:03 +0530

    Created campground model

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
-    res.render('home');
-})
+  res.render("home");
+});
 
 const PORT = 3000 || process.env.PORT;
 app.listen(PORT, () => {
-    console.log(`Server is running on PORT ${PORT}`);
-})
+  console.log(`Server is running on PORT ${PORT}`);
+});
